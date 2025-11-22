a89fde44b03249a0f7e92daa71b067f9ae946c68
solomon54 <solomontsehay54@gmail.com>
2025-11-22 20:43:24 +0300

    importing google fonts

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return(
     <div>
-
-<h1 className=" text-center text-2xl text-gray-200 "> Welcome to the Unspalsh App!</h1>
-    </div>
+      <h1> Welcome to Splash App </h1>
+   </div>
         )
       }
