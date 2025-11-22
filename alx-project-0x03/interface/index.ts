c9d34e6701100d0b0295f8aa6bfacde4d42dfa3c
solomon54 <solomontsehay54@gmail.com>
2025-11-22 21:21:59 +0300

    move the interface to the right directory

--- alx-project-0x03/interface/index.ts
+++ alx-project-0x03/interface/index.ts
@@ -1,20 +1,17 @@
-import Layout from "@/components/layouts/Layout";
 
-interface LayoutProps {
+
+export interface LayoutProps {
   children: ReactNode;
 }
 
 
-interface ButtonProps {
+export interface ButtonProps {
     buttonLabel: string
     buttonSize?: string
     buttonBackgroundColor?: 'red' | 'blue' | 'orange' | 'green'
     action?: () => void
 }
 
-interface PageRouteProps {
+export interface PageRouteProps {
   pageRoute: string
 }
-export default PageRouteProps;
-export  default LayoutProps;
-export  default ButtonProps;
